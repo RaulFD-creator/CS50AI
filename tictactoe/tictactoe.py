7ed9cda5ccf081ea5b9977ff408bfffc27f88a5c
RaulFD-creator <raulfdz9@gmail.com>
2021-06-08 00:12:43 +0200

    Final commitment

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -49,32 +50,97 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    if action not in actions(board):
+        raise Exception("Illegal action")
 
+    table = copy.deepcopy(board)
+    table[action[0]][action[1]] = player(board)
+    return table
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
-
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    elif board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+    else:
+        for i in range(3):
+            if board[i][0] == board[i][1] == board[i][2]:
+                return board[i][0]
+            elif board[0][i] == board[1][i] == board[2][i]:
+                return board[0][i]
+            else:
+                return None
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) is None:
+        count = 0
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == EMPTY:
+                    return False
+        return True
 
+    else:
+        return True
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
-
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+    Max = float("-inf")
+    Min = float("inf")
+
+    if player(board) == "X":
+        return Maximization(board, Max, Min)[1]
+    elif player(board) == "O":
+        return Minimization(board, Max, Min)[1]
+
+def Maximization(board, Max, Min):
+    opt_action = None
+    if terminal(board):
+        return[utility(board), None]
+
+    value = Max
+    for action in actions(board):
+        test = Minimization(result(board, action), Max, Min)[0]
+        Max = max(Max, test)
+        if test > value:
+            value = test
+            opt_action = action
+        if Max >= Min:
+            break
+    return [value, opt_action]
+
+def Minimization(board, Max, Min):
+    opt_action = None
+    if terminal(board):
+        return[utility(board), None]
+    value = Min
+    for action in actions(board):
+        test = Maximization(result(board, action), Max, Min)[0]
+        Min = min(Min, test)
+        if test < value:
+            value = test
+            opt_action = action
+        if Max >= Min:
+            break
+    return [value, opt_action]
